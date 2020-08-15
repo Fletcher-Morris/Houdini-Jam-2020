c0be1a61e4a5e34cacac101909b1dc7324acee28
Fletcher Morris <odmoandtheboy@gmail.com>
2020-08-15 18:12:04 +0100

    More work on planet

--- Assets/Scripts/UpdateMeshCollider.cs
+++ Assets/Scripts/UpdateMeshCollider.cs
@@ -5,13 +5,13 @@ using UnityEngine;
 [ExecuteInEditMode]
 public class UpdateMeshCollider : MonoBehaviour
 {
-public void UpdateCollider()
-{
-Mesh mesh = GetComponent<MeshFilter>().sharedMesh;
-if(mesh == null) return;
-MeshCollider collider = GetComponent<MeshCollider>();
-if(collider == null) return;
-collider.sharedMesh = mesh;
-Debug.Log($"Updated mesh collider on '{gameObject.name}'!");
-}
+	public void UpdateCollider()
+	{
+		Mesh mesh = GetComponent<MeshFilter>().sharedMesh;
+		if(mesh == null) return;
+		MeshCollider collider = GetComponent<MeshCollider>();
+		if(collider == null) collider = gameObject.AddComponent<MeshCollider>();
+		collider.sharedMesh = mesh;
+		Debug.Log($"Updated mesh collider on '{gameObject.name}'!");
+	}
 }
