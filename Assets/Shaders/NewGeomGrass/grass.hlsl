c0b315370c6b674e1cc772e495ec08bbdf1c30da
Fletcher Morris <odmoandtheboy@gmail.com>
2021-04-14 23:13:24 +0100

    More work on updated grass shader

--- Assets/Shaders/NewGeomGrass/grass.hlsl
+++ Assets/Shaders/NewGeomGrass/grass.hlsl
@@ -13,9 +13,9 @@ float3x3 AngleAxis3x3(float angle, float3 axis) {
 	float z = axis.z;
 
 	return float3x3(
-		t * x * x + c, t * x * y - s * z, t * x * z + s * y,
-		t * x * y + s * z, t * y * y + c, t * y * z - s * x,
-		t * x * z - s * y, t * y * z + s * x, t * z * z + c
+	t * x * x + c, t * x * y - s * z, t * x * z + s * y,
+	t * x * y + s * z, t * y * y + c, t * y * z - s * x,
+	t * x * z - s * y, t * y * z + s * x, t * z * z + c
 	);
 }
 
@@ -24,32 +24,39 @@ float3 _LightDirection;
 float4 GetShadowPositionHClip(float3 positionWS, float3 normalWS) {
 	float4 positionCS = TransformWorldToHClip(ApplyShadowBias(positionWS, normalWS, _LightDirection));
 
-#if UNITY_REVERSED_Z
-	positionCS.z = min(positionCS.z, positionCS.w * UNITY_NEAR_CLIP_VALUE);
-#else
-	positionCS.z = max(positionCS.z, positionCS.w * UNITY_NEAR_CLIP_VALUE);
-#endif
+	#if UNITY_REVERSED_Z
+		positionCS.z = min(positionCS.z, positionCS.w * UNITY_NEAR_CLIP_VALUE);
+	#else
+		positionCS.z = max(positionCS.z, positionCS.w * UNITY_NEAR_CLIP_VALUE);
+	#endif
 
 	return positionCS;
 }
 
 float4 WorldToHClip(float3 positionWS, float3 normalWS) {
-#ifdef SHADOW
-	return GetShadowPositionHClip(positionWS, normalWS);
-#else
-	return TransformWorldToHClip(positionWS);
-#endif
+	#ifdef SHADOW
+		return GetShadowPositionHClip(positionWS, normalWS);
+	#else
+		return TransformWorldToHClip(positionWS);
+	#endif
 }
 
 // Variables
 CBUFFER_START(UnityPerMaterial) // Required to be compatible with SRP Batcher
-float4 _Color;
-float4 _Color2;
+float4 _TipColor;
+float4 _RootColor;
 float _Width;
 float _RandomWidth;
 float _WindStrength;
 float _Height;
 float _RandomHeight;
+uint _BladeSegments;
+uint _GrassBlades = 4;
+uint _MinimumGrassBlades = 1;
+half _MaxCameraDistance = 50;
+half _MinCameraDistance = 3;
+half _TrueNormal = 0.5;
+float4 DistortionbObjects[32];
 CBUFFER_END
 
 // Vertex, Geometry & Fragment Shaders
@@ -68,57 +75,114 @@ Varyings vert(Attributes input) {
 	return output;
 }
 
-[maxvertexcount(1 * 2 + 1)]
+#define BLADE_SEGMENTS 2
+
+GeometryOutput GenerateGrassVertex(float4 pos, float3 positionWS, float3 ws, float3 normalWS, float2 uv)
+{
+	GeometryOutput result;
+	result.positionCS = WorldToHClip(positionWS, normalWS);
+
+	return result;
+}
+
+half ObjectDistortion(float3 pos)
+{
+	half total = 0.1;
+	for (int i = 0; i < 32; i++)
+	{
+		half d = distance(pos, DistortionbObjects[i].xyz);
+		total += step(d,DistortionbObjects[i].w);
+	}
+	return clamp(1.0 - total,0.1,1.0);
+}
+
+float invLerp(float from, float to, float value)
+{
+	return (value - from) / (to - from);
+}
+
+[maxvertexcount(1024 / 9)]
 void geom(uint primitiveID : SV_PrimitiveID, triangle Varyings input[3], inout TriangleStream<GeometryOutput> triStream) {
 	GeometryOutput output = (GeometryOutput)0;
 
-	// Construct World -> Tangent Matrix (for aligning grass with mesh normals)
-	float3 normal = input[0].normal;
-	float4 tangent = input[0].tangent;
-	float3 binormal = cross(normal, tangent) * tangent.w;
+	float realCamDist = distance(_WorldSpaceCameraPos,mul(unity_ObjectToWorld, input[0].positionWS));
+	float tooCloseCamDist = clamp(pow(realCamDist/_MinCameraDistance,6),0.1,1.0);
+	float camDist = clamp(invLerp(_MinCameraDistance, _MaxCameraDistance, realCamDist), 0.0, 1.0);
+	int grassBlades = ceil(lerp(_GrassBlades, _MinimumGrassBlades, camDist));
 
-	float3x3 tangentToLocal = float3x3(
+	for(uint blade = 0; blade < grassBlades; blade++)
+	{
+		// Construct World -> Tangent Matrix (for aligning grass with mesh normals)
+		float3 normal = input[0].normal;
+		float4 tangent = input[0].tangent;
+		float3 binormal = cross(normal, tangent) * tangent.w;
+
+		float3x3 tangentToLocal = float3x3
+		(
 		tangent.x, binormal.x, normal.x,
 		tangent.y, binormal.y, normal.y,
 		tangent.z, binormal.z, normal.z
 		);
 
-	float3 positionWS = input[0].positionWS;
+		float r1 = rand(mul(unity_ObjectToWorld, input[0].positionWS).xyz * (blade + 1));
+		float r2 = rand(mul(unity_ObjectToWorld, input[1].positionWS).xyz * (blade + 1));
+		float r3 = rand(mul(unity_ObjectToWorld, input[2].positionWS).xyz * (blade + 1));
+		float3 positionWS = (1 - sqrt(r1)) * input[0].positionWS + (sqrt(r1) * (1 - r2)) * input[1].positionWS + (sqrt(r1) * r2) * input[2].positionWS;
+		normal = normalize(lerp(normal, normalize(positionWS), _TrueNormal));
+		r1 = r1 * 2.0 - 1.0;
+		r2 = r2 * 2.0 - 1.0;
+		r3 = r3 * 2.0 - 1.0;
 
-	float r = rand(positionWS.xyz);
-	float3x3 randRotation = AngleAxis3x3(r * TWO_PI, float3(0, 0, 1));
+		float r = rand(positionWS.xyz * (blade + 1));
+		float3x3 randRotation = AngleAxis3x3(r * TWO_PI, float3(0, 0, 1));
 
-	// Wind (based on sin / cos, aka a circular motion, but strength of 0.1 * sine)
-	float2 wind = float2(sin(_Time.y + positionWS.x * 0.5), cos(_Time.y + positionWS.z * 0.5)) * _WindStrength * sin(_Time.y + r);
-	float3x3 windMatrix = AngleAxis3x3((wind * PI).y, normalize(float3(wind.x, wind.y, 0)));
+		half heightFactor = _Height;
+		heightFactor *- _RandomHeight * (rand(positionWS.yxz) - 0.5);
+		heightFactor *= ObjectDistortion(positionWS);
+		heightFactor *= tooCloseCamDist;
 
-	float3x3 transformMatrix = mul(tangentToLocal, randRotation);
-	float3x3 transformMatrixWithWind = mul(mul(tangentToLocal, windMatrix), randRotation);
+		// Wind (based on sin / cos, aka a circular motion, but strength of 0.1 * sine)
+		float2 wind = float2(sin(_Time.y + positionWS.x * 0.5), cos(_Time.y + positionWS.z * 0.5)) * _WindStrength * sin(_Time.y + r);
+		float3x3 windMatrix = AngleAxis3x3((wind * PI).y, normalize(float3(wind.x, wind.y, 0)));
 
-	float bend = rand(positionWS.xyz) - 0.5;
-	float width = _Width + _RandomWidth * (rand(positionWS.zyx) - 0.5);
-	float height = _Height + _RandomHeight * (rand(positionWS.yxz) - 0.5);
+		float3x3 transformMatrix = mul(tangentToLocal, randRotation);
+		float3x3 transformMatrixWithWind = mul(mul(tangentToLocal, windMatrix), randRotation);
 
-	float3 normalWS = mul(transformMatrix, float3(0, 1, 0)); //?
+		float bend = rand(positionWS.xyz) - 0.5;
+		float width = _Width + _RandomWidth * (rand(positionWS.zyx) - 0.5);
 
-	// Handle Geometry
+		float3 normalWS = mul(transformMatrix, float3(0, 1, 0)); //?
 
-	// Base 2 vertices
-	output.positionWS = positionWS + mul(transformMatrix, float3(width, 0, 0));
-	output.positionCS = WorldToHClip(output.positionWS, normalWS);
-	output.uv = float2(0, 0);
-	triStream.Append(output);
+		if(heightFactor <= 0.005) continue;
+		//	If grass is too short, skip to next blade.
 
-	output.positionWS = positionWS + mul(transformMatrix, float3(-width, 0, 0));
-	output.positionCS = WorldToHClip(output.positionWS, normalWS);
-	output.uv = float2(0, 0);
-	triStream.Append(output);
+		for(uint seg = 0; seg < _BladeSegments; seg++)
+		{
+			half lerpVal = seg / (half)_BladeSegments;
+			half lerpedWidth = width * (1 - lerpVal);
+			half lerpedHeight = heightFactor * lerpVal;
+			half lerpedBend = bend * lerpVal;
+			lerpedBend *= lerpedBend;
 
-	// Final vertex at top of blade
-	output.positionWS = positionWS + mul(transformMatrixWithWind, float3(0, bend, height));
-	output.positionCS = WorldToHClip(output.positionWS, normalWS);
-	output.uv = float2(0, 1);
-	triStream.Append(output);
+			float3 segPos = positionWS + (normal * lerpedHeight);
 
-	triStream.RestartStrip();
+			output.positionWS = segPos + mul(transformMatrixWithWind, float3(lerpedWidth, bend, lerpedHeight));
+			output.positionCS = WorldToHClip(output.positionWS, normalWS);
+			output.uv = float2(0, lerpVal);
+			triStream.Append(output);
+
+			output.positionWS = segPos + mul(transformMatrixWithWind, float3(-lerpedWidth, bend, lerpedHeight));
+			output.positionCS = WorldToHClip(output.positionWS, normalWS);
+			output.uv = float2(0, lerpVal);
+			triStream.Append(output);
+		}
+		
+		// Final vertex at top of blade
+		output.positionWS = positionWS + mul(transformMatrixWithWind, float3(0, bend, heightFactor));
+		output.positionCS = WorldToHClip(output.positionWS, normalWS);
+		output.uv = float2(0, 1);
+		triStream.Append(output);
+
+		triStream.RestartStrip();
+	}
 }
